259f9c2754ebec4e0401875f133b4a026b9cca93
anjana96n <94234293+anjana96n@users.noreply.github.com>
2023-02-08 21:36:53 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-h-screen py-6 px-12">
+      <div className="sm:grid grid-cols-5 gap-4">
+        <div className="col-span-2 border border-1 border-gray-400 rounded-lg"></div>
+        <div className="col-span-3">
+          <div className="border border-1 rounded-lg border-gray-400"></div>
+        </div>
+      </div>
     </div>
   );
 }
